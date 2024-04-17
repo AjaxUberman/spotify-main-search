94dbe10bc3b709b0f7667acf8db394cb1c77c134
AjaxUberman <bariskayikcidev@gmail.com>
2024-04-17 17:47:59 +0300

    fixes

--- src/components/SongItem.js
+++ src/components/SongItem.js
@@ -12,6 +12,7 @@ const SongItem = ({ item }) => {
   return (
     <div className="">
       <div
+        onClick={updateCurrent}
         key={item.id}
         className="rounded-xl hover:bg-active px-3 py-2 group relative"
       >
@@ -25,7 +26,7 @@ const SongItem = ({ item }) => {
         <p className="text-graytext">{item.description}</p>
         <FaCirclePlay
           onClick={updateCurrent}
-          className=" opacity-0 group-hover:opacity-100 duration-200	 transition ease-in group-hover:-translate-y-24 md:-translate-y-20  md:translate-x-40  md:bottom-2 md:text-5xl text-3xl absolute right-6 -bottom-4 text-primarygreen bg-black rounded-full"
+          className=" opacity-0 group-hover:opacity-100 duration-200 transition ease-in group-hover:-translate-y-24  md:text-5xl text-3xl absolute right-6 -bottom-4 text-primarygreen bg-black rounded-full"
         />
       </div>
     </div>
